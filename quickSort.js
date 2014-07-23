773cf71964c766fa100e26306cf4246441a54ac6
sunorry <sunorry@gmail.com>
2014-07-23 18:14:40 +0800

    .

--- quickSort.js
+++ quickSort.js
@@ -5,7 +5,7 @@ function quickSort(arr) {
   }
 
   var num = Math.floor(arr.length/2); //找到中间位置
-  var numValue = arr.splice(num ,1); //从数组里提出来
+  var numValue = arr.splice(num ,1)[0]; //从数组里提出来
 
   var left = [];
   var right = [];
