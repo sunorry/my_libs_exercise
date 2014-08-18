056f9f5cbc59cb0c7ca5a389dddaac49dc5c2224
wangxing.sun <wangxing.sun@qunar.com>
2014-08-18 19:21:19 +0800

    .

---
+++ underscore-tpl.js
@@ -0,0 +1,29 @@
+_.template(templateString, [data], [settings])
+//1
+var compiled = .template("hello: <%= name %>");
+compiled({name: 'sunorry'});
+
+//2 <%%>分别包住 function 的左边和右边，中间数据再包，标签不用包
+var list = "<% _.each(people, function(name){ %> <li><%= name %></li> <%});%>";
+_.template(list, {people: [1,2,3,4,5,6,7]});
+
+//3 如果里面有需要转移的，把 = 换成 -
+var template = _.template("<b><%- value %></b>");
+_.template({value:'<script>'});
+
+
+
+var context = {
+    people: [
+      { firstName: "Yehuda", lastName: "Katz" },
+      { firstName: "Carl", lastName: "Lerche" },
+      { firstName: "Alan", lastName: "Johnson" }
+    ]
+  };
+
+var list = "<ul>
+              <% _.each(people, function(name){ %>
+                <li><%= name.firstName %> <%= name.lastName %> </li>
+              <%});%>
+            </ul>";
+_.template(list, {people: context.people});
