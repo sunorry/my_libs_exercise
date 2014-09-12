6394474317cc2f86900c3b55aadc779e1991cb67
sunorry <sunorry@gmail.com>
2014-09-12 12:22:31 +0800

    debug the underscore template

--- underscore-tpl.js
+++ underscore-tpl.js
@@ -1,6 +1,6 @@
 _.template(templateString, [data], [settings])
 //1
-var compiled = .template("hello: <%= name %>");
+var compiled = _.template("hello: <%= name %>");
 compiled({name: 'sunorry'});
 
 //2 <%%>分别包住 function 的左边和右边，中间数据再包，标签不用包
@@ -20,10 +20,11 @@ var context = {
       { firstName: "Alan", lastName: "Johnson" }
     ]
   };
-
+//把空格去掉可运行成功
 var list = "<ul>
               <% _.each(people, function(name){ %>
                 <li><%= name.firstName %> <%= name.lastName %> </li>
               <%});%>
             </ul>";
-_.template(list, {people: context.people});
+var template = _.template(list);
+template({people: context.people})
